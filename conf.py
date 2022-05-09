fd1d1856466374dc45a410048da9458df89e4fa1
Demon1709 <104796840+Demon1709@users.noreply.github.com>
2022-05-09 15:08:09 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "3be954be110a796e8c0b18f3604a5f2c"
-api_id = 16356512
-bot_token = "5302923436:AAEC8JHVHj6OCGjl27ImDeIorGpZ9TJiljc"
-bot_admin_group = -1001581671258
+api_hash = "a5007babf6f3e96c9b51564356b64c30"
+api_id = 11639974
+bot_token = "5354124173:AAHm_1mXAex0fsi7wTcQm2kxrJb_oXd10pE"
+bot_admin_group = -1001739674710
