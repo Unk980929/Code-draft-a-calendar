5bef3323738cfb72d5c54b190d97045a2fbb8852
Ruben323h <99520739+Ruben323h@users.noreply.github.com>
2022-05-09 17:36:41 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "a5007babf6f3e96c9b51564356b64c30"
+api_hash = "5312723798:AAHzLlDwIRH5Sa1Ot-e4ZTUAoGQNDLjQ5lE"
 api_id = 11639974
 bot_token = "5354124173:AAHm_1mXAex0fsi7wTcQm2kxrJb_oXd10pE"
-bot_admin_group = -1001739674710
+bot_admin_group = -646921878
